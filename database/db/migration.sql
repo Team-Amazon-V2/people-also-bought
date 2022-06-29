db776a08ec6f2e1270bba18c0692c3520084f909
magdielrosario97 <magdielrosario97@gmail.com>
2022-06-29 15:21:13 -0500

    added asin column to db

--- database/db/migration.sql
+++ database/db/migration.sql
@@ -6,6 +6,7 @@ DROP TABLE IF EXISTS related_products;
 
 CREATE TABLE related_products(
     id SERIAL UNIQUE PRIMARY KEY,
+    asin_id VARCHAR,
     picture TEXT,
     product_name TEXT,
     rating NUMERIC,
@@ -14,9 +15,8 @@ CREATE TABLE related_products(
     price NUMERIC
 );
 
-INSERT INTO related_products (picture, product_name, rating, reviews, category, price)
+INSERT INTO related_products (asin_id, picture, product_name, rating, reviews, category, price)
     VALUES
-    ('https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/ipad-pro-11-select-wifi-silver-202104_FMT_WHH?wid=1945&hei=2000&fmt=jpeg&qlt=95&.v=1617126626000',
+    ('B09V3HN1KC', 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/ipad-pro-11-select-wifi-silver-202104_FMT_WHH?wid=1945&hei=2000&fmt=jpeg&qlt=95&.v=1617126626000',
     '2021 Apple 11-inch iPad Pro (Wi-Fi, 128GB) - Silver',
     4.9, 5180, 'Electronics', 749.00);
-
